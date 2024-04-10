792b8e7fb4594dfc8cdda35e2cf856e9b41b23bb
Chandan <94438598+Chandan157@users.noreply.github.com>
2024-04-10 23:00:34 +0530

    Recomendation component added

--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -1,9 +1,11 @@
 import PlayVideo from "../../Components/PlayVideo/PlayVideo";
+import Recommended from "../../Components/Recommended/Recommended";
 import "./Video.css";
 const Video = () => {
   return (
     <div className="play-container">
       <PlayVideo />
+      <Recommended/>
     </div>
   );
 };
